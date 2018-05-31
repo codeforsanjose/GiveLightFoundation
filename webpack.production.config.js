1f882e66f2fd190c13b46dd3c3b00a13966a2777
Joseph <jrichardson@jmstudios.net>
2018-05-31 10:38:56 -0700

    add correct config to prod webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,13 +13,25 @@ module.exports = {
     filename: './bundle.js'
   },
   module: {
-    loaders: [
-      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
-      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' }
+    rules: [
+      { test: /\.css$/, include: path.resolve(__dirname, 'app'), use: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, include: [path.resolve(__dirname, 'app') ], use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.json?$/, include: path.resolve(__dirname, 'config'), use: 'json-loader' },
+      {
+        test: /\.(png|jp(e*)g|svg)$/, include: path.resolve(__dirname, 'images'),
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', 'json', '.scss']
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
